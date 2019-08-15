ebf71671beff02a64cfc829e8ccbcf1c74a54f3e
jmccasusi <jmccasusi@gmail.com>
2019-08-15 18:31:57 -0400

    fixed CORS

--- bookmark_api/server.js
+++ bookmark_api/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express ();
 const Port = 3003;
 
 const bookmarkController = require('./controllers/bookmark')
+
 // Middleware
 app.use(express.json());
+
+// CORS
+const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
 app.use('/bookmark', bookmarkController)
-
-
 
 // Error / Disconnection
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
