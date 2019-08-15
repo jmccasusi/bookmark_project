d69e558a19ac8b8e009656a547c2b17a76faa4a7
jmccasusi <jmccasusi@gmail.com>
2019-08-15 16:57:36 -0400

    added seed data and seed route

--- bookmark_api/controllers/bookmark.js
+++ bookmark_api/controllers/bookmark.js
@@ -2,10 +2,28 @@ const express = require('express')
 const bookmark = express.Router()
 const Bookmark = require('../model/bookmark')
 
+// Seed data
+const BookmarkSeedData = require('../model/seedData.js')
+
+// seed route - only use once
+// bookmark.get('/seed', (req,res) => {
+//     Bookmark.create(BookmarkSeedData, (err, createdBookmarks) => {
+//         res.redirect('/bookmark');
+//     })
+// })
 
 // index
 bookmark.get('/', (req,res)=>{
-    res.send("hello")
+    Bookmark.find({}, (err, foundBookmarks) => {
+        if (err) {
+            res.status(400).json({
+                error: error.message
+            });
+        }
+        res.status(200).json(foundBookmarks);
+    })
 })
+
+// 
 
 module.exports = bookmark
