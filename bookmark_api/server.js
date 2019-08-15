db9aee72a26f739a00e9139e938ec96e460fd412
jmccasusi <jmccasusi@gmail.com>
2019-08-15 17:58:36 -0400

    changed backend port to 3003

--- bookmark_api/server.js
+++ bookmark_api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express ();
-const Port = 3000
+const Port = 3003;
 
 const bookmarkController = require('./controllers/bookmark')
 // Middleware
