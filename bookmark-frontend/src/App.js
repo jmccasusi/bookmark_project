b026d3f5c82999cb5eb2398f41d03d2b24e2bc1a
jmccasusi <jmccasusi@gmail.com>
2019-08-15 18:03:45 -0400

    get bookmark data function done

--- bookmark-frontend/src/App.js
+++ bookmark-frontend/src/App.js
@@ -5,16 +5,46 @@ import HeaderComponent from './components/HeaderComponent';
 import NewFormComponent from './components/NewFormComponent';
 import ShowComponent from './components/ShowComponent';
 
-function App() {
-  return (
-    <div className="App">
-        <HeaderComponent/>
-        <hr/>
-        <NewFormComponent/>
-        <hr/>
-        <ShowComponent/>
-    </div>
-  );
+let baseURL = process.env.REACT_APP_BASEURL;
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: []
+    }
+    this.getBookmarks = this.getBookmarks.bind(this);
+  }
+
+  async getBookmarks() {
+    const response = await axios(`${baseURL}/bookmark`);
+    const data = response.data;
+    this.setState({
+      bookmarks: data
+    })
+  }
+
+  async componentDidMount() {
+    this.getBookmarks();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <HeaderComponent />
+        <hr />
+        <NewFormComponent />
+        <hr />
+        <ShowComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
