fe41ed96fda1f925bb3b2ec257e3cf76fa028166
jmccasusi <jmccasusi@gmail.com>
2019-08-16 15:02:35 -0400

    modified edit component

--- bookmark-frontend/src/components/EditFormComponent.js
+++ bookmark-frontend/src/components/EditFormComponent.js
@@ -4,7 +4,15 @@ class EditFormComponent extends React.Component {
     render() {
         return(
             <div>
-                This is the Edit Form component.
+                <h6>Edit this Bookmark</h6>
+                <form>
+
+                <div class="form-group row">
+                    <input class="form-control col" type="text" name="title" value="" placeholder="website" onChange={""}/>
+                    <input class="form-control col" type="text" name="url" value={""} placeholder="http://" onChange={""}/>
+                    <input type="submit"  class="btn btn-primary" value="Save"/>
+                </div>
+                </form>
             </div>
         )
     }
